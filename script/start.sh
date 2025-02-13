7cd893b6704458b2532faf076380de8ce6a469c1
sibi000 <riccardosibilia0@gmail.com>
2025-02-13 11:25:19 +0100

    commit2.0

--- script/start.sh
+++ script/start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # run ryu controller
-# cd ~/comnetsemu/Project_2/topologyVisualizer
 cd /media/projects/Project_net2/topologyVisualizer
 ryu-manager --observe-links gui_start.py controller.py
 sleep 1
