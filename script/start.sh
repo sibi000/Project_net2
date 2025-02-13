647dc709f5749afbdcbe827affc6fd9d656a8f9d
Maxmichelini <massimilianomichelini01@gmail.com>
2025-02-13 11:20:39 +0100

    total push

--- script/start.sh
+++ script/start.sh
@@ -2,6 +2,7 @@
 
 # run ryu controller
 # cd ~/comnetsemu/Project_2/topologyVisualizer
-cd /media/projects/Project_2/topologyVisualizer
+# cd /media/projects/Project_2/topologyVisualizer
+cd ~/comnetsemu/Project_net2/topologyVisualizer
 ryu-manager --observe-links gui_start.py controller.py
 sleep 1
