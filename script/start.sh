033db74fb211f2cdc1344f860c970fff8446ebf5
sibi000 <riccardosibilia0@gmail.com>
2025-02-13 10:42:33 +0100

    modification to path

--- script/start.sh
+++ script/start.sh
@@ -2,6 +2,6 @@
 
 # run ryu controller
 # cd ~/comnetsemu/Project_2/topologyVisualizer
-cd /media/projects/Project_2/topologyVisualizer
+cd /media/projects/Project_net2/topologyVisualizer
 ryu-manager --observe-links gui_start.py controller.py
 sleep 1
